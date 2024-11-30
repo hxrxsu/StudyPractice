ecda2c7dc62a88a2c560bd0456b2887bfc8d18cd
HXRXSU <HXRXSU@DESKTOP-5LLNC12>
2024-11-30 16:24:17 +0400

    Add project files.

---
+++ StudyPractice/Auth.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudyPractice
+{
+    /// <summary>
+    /// Interaction logic for Auth.xaml
+    /// </summary>
+    public partial class Auth : Page
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+
+        public string resultCurrentUser;
+
+        private void BN_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var currentuserauth = db.Users.FirstOrDefault(u => u.Login == TB_Login.Text && u.Password == TB_Password.Text);
+
+
+                if (currentuserauth != null)
+                {
+                    if(currentuserauth.Role == "Администратор")
+                    {
+                        MessageBox.Show("Вход выполнен!", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Information);
+                        resultCurrentUser = $"Здравствуйте!: {currentuserauth.Name} Ваша роль: {currentuserauth.Role}";
+                        NavigationService.Navigate(new AdminPanelPage(resultCurrentUser));
+                    }
+                    else if(currentuserauth.Role == "Участник")
+                    {
+                        MessageBox.Show("Вход выполнен!", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Information);
+                        resultCurrentUser = $"Здравствуйте!: {currentuserauth.Name} Ваша роль: {currentuserauth.Role}";
+                        NavigationService.Navigate(new MemberEventPage(resultCurrentUser, currentuserauth));
+                    }
+                    else if(currentuserauth.Role == "Модератор")
+                    {
+                        MessageBox.Show("Вход выполнен!", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Information);
+                        NavigationService.Navigate(new ModeratorEventPage(currentuserauth));
+                    }
+                    else if(currentuserauth.Role == "Жюри")
+                    {
+                        MessageBox.Show("Вход выполнен!", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Information);
+                        NavigationService.Navigate(new JuryEventPage(currentuserauth));
+                    }
+                    else if(currentuserauth.Role == "Организатор")
+                    {
+                        MessageBox.Show("Вход выполнен!", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Information);
+                        resultCurrentUser = $"Здравствуйте!: {currentuserauth.Name} Ваша роль: {currentuserauth.Role}";
+                        NavigationService.Navigate(new OrganizerEventPage(resultCurrentUser));
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Введите корректные данные или зарегистрируйтесь!", "Авторизация", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                }
+            }
+        }
+
+        private void BN_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Reg());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void TB_Password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TB_Password.Text = "";
+        }
+
+        private void TB_Login_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TB_Login.Text = "";
+        }
+    }
+}
---
+++ StudyPractice/Reg.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace StudyPractice
+{
+    /// <summary>
+    /// Interaction logic for Reg.xaml
+    /// </summary>
+    public partial class Reg : Page
+    {
+        public Reg()
+        {
+            InitializeComponent();
+        }
+
+        private void TB_Login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BN_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Auth());
+        }
+
+        private void BN_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var login = TB_Login.Text;
+                var password = TB_Password.Text;
+
+                try
+                {
+                    if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password) && string.IsNullOrEmpty(TB_FIO.Text) || string.IsNullOrEmpty(DP_DateBirth.SelectedDate.Value.ToString()))
+                    {
+                        
+                    }
+                    else
+                    {
+                        User _newUser = new User { Name = TB_FIO.Text, Login = login, Password = password, DateOfBirth = DP_DateBirth.SelectedDate.Value, PhoneNumber = Convert.ToInt32(TB_PhoneNumber.Text), Email = TB_Email.Text, Role = "Администратор" };
+                        db.Users.Add(_newUser);
+                        db.SaveChanges();
+                        MessageBox.Show("Вы успешно зарегистрировались!", "Регистрация", MessageBoxButton.OK, MessageBoxImage.Information);
+                        NavigationService.Navigate(new Auth());
+                    }
+                }
+                catch(Exception ex) { MessageBox.Show("Заполните логин, пароль, фио и дату рождения для регистрации!", "Регистрация", MessageBoxButton.OK, MessageBoxImage.Error); }     
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void TB_FIO_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TB_FIO.Text = "";
+        }
+
+        private void TB_Email_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TB_Email.Text = "";
+        }
+
+        private void TB_PhoneNumber_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TB_PhoneNumber.Text = "";
+        }
+
+        private void TB_Login_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TB_Login.Text = "";
+        }
+
+        private void TB_Password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TB_Password.Text = "";
+        }
+    }
+}
